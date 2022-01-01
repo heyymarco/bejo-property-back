10b0e0ade565a2bc583d99f3d1b0d2b759a61b2d
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-01-01 22:08:01 +0700

    updated

--- schemas/category.js
+++ schemas/category.js
@@ -8,6 +8,19 @@ export default {
             name: 'title',
             title: 'judul',
             type: 'string',
+
+            validation: Rule => Rule.required().error('harus diisi'),
+        },
+        {
+            name: 'slug',
+            title: 'Slug',
+            type: 'slug',
+            options: {
+                source: 'title',
+                maxLength: 20,
+            },
+
+            validation: Rule => Rule.required().error('harus diisi'),
         },
         {
             name: 'description',
